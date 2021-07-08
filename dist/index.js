8c2576ec23d1603a1bd042eac65467bc871e0a0b
savaukr <sava_ukr@ukr.net>
2021-07-08 17:41:26 +0300

    get and post request are working

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,27 @@
-console.log('hello');
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var express_1 = __importDefault(require("express"));
+var app = express_1["default"]();
+var PORT = 5000;
+app.get('/list/dog/images', function (req, res) {
+    // console.log(request)
+    res.send('response from get');
+});
+app.post('/upload/dog/image', function (req, res) {
+    console.log(req.body);
+    res.send(" response from post ");
+});
+function start() {
+    try {
+        app.listen(PORT, function () { console.log("App has been started on port " + PORT + "..."); });
+    }
+    catch (e) {
+        console.log("Server Error", e.message);
+        process.exit(1);
+    }
+}
+start();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('hello')
+import express, { Express, RequestHandler }  from 'express';
+
+const app: Express = express();
+const PORT = 5000;
+app.get('/list/dog/images', (req, res) => {
+    // console.log(request)
+    res.send('response from get');
+});
+
+app.post('/upload/dog/image', (req, res) => {
+    console.log(req.body)
+    res.send(" response from post ")
+})
+
+function start() {
+	try {
+		app.listen(PORT, () => {console.log(`App has been started on port ${PORT}...`)})
+	} catch(e) {
+		console.log("Server Error", e.message)
+		process.exit(1)
+	}
+}
+
+start()
